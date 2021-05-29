0e33e3a3a3f5276fcfdf0712b30508c61e49c369
yuliamed <yulinwo.17@gmail.com>
2021-05-29 15:15:26 +0300

    feat: implement task 1 - convertToHappy

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,22 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        if (sadArray.size <= 2) {
+            return sadArray
+        }
+        var happyArray = sadArray.toMutableList()
+        var work = true;
+        while (work) {
+            var innerWork = false
+            for (i in 1 until happyArray.lastIndex) {
+                if (happyArray[i] > happyArray[i - 1] + happyArray[i + 1]) {
+                    happyArray.removeAt(i)
+                    innerWork = true
+                    break
+                }
+            }
+            if (!innerWork) break
+        }
+        return happyArray.toIntArray()
     }
 }
