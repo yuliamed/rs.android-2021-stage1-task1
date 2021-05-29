ef190df2ea78540aba21e31917dbaa341521f4c8
yuliamed <yulinwo.17@gmail.com>
2021-05-29 15:19:11 +0300

    feat: implement task 3 - stringParser

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,60 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val subStrings = arrayListOf<String>()
+        var count = 0
+        var indexStart = 0
+        for (i in inputString.indices) {
+            when (inputString[i]) {
+                '<' -> {
+                    indexStart = i + 1
+                    for (j in i..inputString.lastIndex) {
+                        if (inputString[j] == '<') {
+                            count++
+                        }
+                        if (inputString[j] == '>') {
+                            count--
+                            if (count == 0) {
+                                subStrings.add(inputString.substring(indexStart until j))
+                                break
+                            }
+                        }
+                    }
+                }
+                '[' -> {
+                    indexStart = i + 1
+                    for (j in i..inputString.lastIndex) {
+                        if (inputString[j] == '[') {
+                            count++
+                        }
+                        if (inputString[j] == ']') {
+                            count--
+                            if (count == 0) {
+                                subStrings.add(inputString.substring(indexStart until j))
+                                break
+                            }
+                        }
+                    }
+                }
+                '(' -> {
+                    indexStart = i + 1
+                    for (j in i..inputString.lastIndex) {
+                        if (inputString[j] == '(') {
+                            count++
+                        }
+                        if (inputString[j] == ')') {
+                            count--
+                            if (count == 0) {
+                                subStrings.add(inputString.substring(indexStart until j))
+                                break
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return subStrings.toTypedArray()
     }
 }
+
